ac8d68beb1881efdf98a0df240e5bc35dd916c36
vikramT75 <vikramaditya.kr03@gmail.com>
2026-02-18 01:40:47 +0530

    P1D3

--- ingestion/binance_stream.py
+++ ingestion/binance_stream.py
@@ -1,9 +1,20 @@
 import asyncio
 import json
 import websockets
-from datetime import datetime
+from datetime import datetime, timezone
+from kafka import KafkaProducer
 
 BINANCE_WS_URL = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    linger_ms=5,
+    acks="all",
+)
+
+TOPIC = "crypto_trades"
+
 
 async def stream_prices():
     async with websockets.connect(BINANCE_WS_URL) as ws:
@@ -12,12 +23,22 @@ async def stream_prices():
         while True:
             message = await ws.recv()
             data = json.loads(message)
+            
+            trade_time_utc = datetime.fromtimestamp(
+                data["T"] / 1000, tz=timezone.utc
+            ).strftime("%Y-%m-%d %H:%M:%S")
 
-            price = data["p"]
-            quantity = data["q"]
-            trade_time = data["T"]
-            human_time = datetime.fromtimestamp(trade_time / 1000)
-            print(f"Price: {price} | Qty: {quantity} | Time: {human_time}")
+            trade = {
+                "symbol": data["s"],
+                "price": data["p"],
+                "quantity": data["q"],
+                "trade_time": trade_time_utc,
+            }
+
+            producer.send(TOPIC, trade)
+
+            print(f"Sent to Kafka: {trade}")
+
 
 if __name__ == "__main__":
     asyncio.run(stream_prices())
