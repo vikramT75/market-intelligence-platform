8e6c8cc909ff736cd9907bda8cb043947d17e8d4
vikramT75 <vikramaditya.kr03@gmail.com>
2026-02-22 20:09:04 +0530

    D5 Persist Metrics into PostgreSQL

--- stream-processing/metrics_worker.py
+++ stream-processing/metrics_worker.py
@@ -2,10 +2,13 @@ import json
 from kafka import KafkaConsumer, KafkaProducer
 from collections import deque
 import statistics
+import psycopg2
+from datetime import datetime
 
 TRADES_TOPIC = "crypto_trades"
 METRICS_TOPIC = "crypto_metrics"
 
+# Kafka consumer
 consumer = KafkaConsumer(
     TRADES_TOPIC,
     bootstrap_servers="localhost:9092",
@@ -13,10 +16,20 @@ consumer = KafkaConsumer(
     value_deserializer=lambda v: json.loads(v.decode("utf-8")),
 )
 
+# Kafka producer (for derived metrics topic)
 producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
 )
+
+# PostgreSQL connection
+conn = psycopg2.connect(
+    host="localhost",
+    database="marketdb",
+    user="market",
+    password="marketpass"
+)
+cursor = conn.cursor()
 
 print("Metrics worker started...\n")
 
@@ -29,10 +42,8 @@ for message in consumer:
     price = float(trade["price"])
     price_window.append(price)
 
-    # Rolling average
     rolling_avg = sum(price_window) / len(price_window)
 
-    # Return
     if previous_price is not None:
         simple_return = (price - previous_price) / previous_price
     else:
@@ -40,11 +51,12 @@ for message in consumer:
 
     previous_price = price
 
-    # Volatility
     if len(price_window) > 1:
         volatility = statistics.stdev(price_window)
     else:
         volatility = 0.0
+
+    trade_time = datetime.strptime(trade["trade_time"], "%Y-%m-%d %H:%M:%S")
 
     metrics = {
         "symbol": trade["symbol"],
@@ -52,10 +64,29 @@ for message in consumer:
         "rolling_avg_20": rolling_avg,
         "return": simple_return,
         "volatility": volatility,
-        "trade_time": trade["trade_time"],
+        "trade_time": trade_time.strftime("%Y-%m-%d %H:%M:%S"),
     }
 
-    # Publish computed metrics
+    # Publish to Kafka derived topic
     producer.send(METRICS_TOPIC, metrics)
 
-    print(f"Published metrics: {metrics}")
+    # Insert into PostgreSQL
+    cursor.execute(
+        """
+        INSERT INTO crypto_metrics 
+        (symbol, price, rolling_avg_20, return, volatility, trade_time)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """,
+        (
+            metrics["symbol"],
+            metrics["price"],
+            metrics["rolling_avg_20"],
+            metrics["return"],
+            metrics["volatility"],
+            trade_time
+        )
+    )
+
+    conn.commit()
+
+    print(f"Stored in DB + Published: {metrics}")
